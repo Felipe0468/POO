3400a51fefacbd92e834eddc8395bee170d9c1c6
Felipe0468 <>
2021-04-22 22:25:57 +0000

    Circuferencia

--- Programa4/PrimerasFiguras.java
+++ Programa4/PrimerasFiguras.java
@@ -7,7 +7,7 @@ public class PrimerasFiguras extends JApplet {
   public void paint (Graphics g) {
     g.drawRect(30,30,80,40);
     g.drawOval(120,30,50,50);
-    g.setColor(Color.black);
+    g.setColor(Color.GREEN);
     g.fillRect(30,100,80,40);
     g.fillOval(120,100,50,50);
     g.drawLine(30,160,130,170);
